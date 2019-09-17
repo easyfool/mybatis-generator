c81b7869fe24218278fbf9bb2a00f9c8ebdcb359
wangfeng <wangfengbabe@163.com>
2019-09-17 23:05:04 +0800

    init

---
+++ src/main/java/com/github/wangfeng/mybatis_generator/IbuyMybatisgeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.github.wangfeng.mybatis_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbuyMybatisgeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IbuyMybatisgeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/wangfeng/mybatis_generator/IbuyMybatisgeneratorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.wangfeng.mybatis_generator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IbuyMybatisgeneratorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
